3a427340f4e363dc60afe4c5e8824666cbb278de
llbasull <bashumoulik@gmail.com>
2024-05-07 16:41:29 +0530

    connected db twice

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose
-  .connect(
-    `mongodb+srv://abhinav:${process.env.password}@ecommercev1.4k1rym2.mongodb.net/?retryWrites=true&w=majority&appName=ecommerceV1`
-  )
-  .then(() => {
-    console.log("Connected to the database");
-  });
 
 const schema = new mongoose.Schema({
   productName: String,
