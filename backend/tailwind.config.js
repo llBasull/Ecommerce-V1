48d165f4353bc23ed3388c5f5f14340cb67a63fc
llSiddharthll <siddharthgreat443@gmail.com>
2024-05-09 10:06:56 +0530

    done

---
+++ backend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,css,js}", "./views/**/*/.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
